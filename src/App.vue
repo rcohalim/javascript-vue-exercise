d18898c04d99dd60a5ec68b62e83b33bbec983a8
riccop13 <riccopratama30@gmail.com>
2022-06-08 21:49:51 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <div id="app">
+    <div class="container-lg container">
+      <div class="title-container">
+        <h1 class="title text-center">Good Morning!</h1>
+      </div>
+      
+      <div class="form-container d-flex mt-3 mb-5 justify-content-end">
+        <form class="search-location me-3" v-on:submit.prevent="getWeather">
+          <input
+            type="text"
+            class="form-control text-muted form-rounded p-2 shadow-sm"
+            placeholder="Search City..."
+            v-model="citySearch"
+            autocomplete="off"
+          />
+        </form>
+
+        <input type="submit" class="btn btn-primary align-middle" value="Search" v-on:click.prevent="getWeather">
+      </div>
+      
+      <div class="flex-row">
+        <!-- <CityCard /> -->
+      </div>
+
+      <CityCard/>
+      <WeatherView/>
+
+
+      <div class="card rounded p-5 my-3 shadow-lg back-card overflow-hidden">
+        <h2 class="text-start">{{weather.cityName}}</h2>
+        <div class="row mt-5">
+          <h5 class="text-start">Today</h5>
+            
+          <div class="container-fluid mt-3">
+            <div class="row">
+              <!-- should be iterate -->
+              <div class="col"> 
+                <h5>3 AM</h5>
+                <img src="" alt="">
+                <h5>25C</h5>
+              </div>
+              <div class="col">2</div>
+              <div class="col">3</div>
+              <div class="col">4</div>
+              <div class="col">5</div>
+              <div class="col">6</div>
+              <div class="col">7</div>
+              <div class="col">8</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row mt-3">
+          <h5 class="text-start">Tomorrow</h5>
+          
+          <div class="container-fluid mt-3">
+            <div class="row">
+              <div class="col">1</div>
+              <div class="col">2</div>
+              <div class="col">3</div>
+              <div class="col">4</div>
+              <div class="col">5</div>
+              <div class="col">6</div>
+              <div class="col">7</div>
+              <div class="col">8</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CityCard from './components/CityCard.vue';
+import WeatherView from './components/WeatherView.vue';
+
+export default {
+  data() {
+    return {
+      citySearch: "",
+      weather: {
+        cityName:"Amsterdam",
+        country:"NL",
+        today_temp: 12,
+        description: "Clouds everywhere",
+      },
+    };
+  },
+  methods: {
+    getWeather: async function() {
+      console.log(this.citySearch);
+      const api_key = 'ceede1b6b3f225c1d5656cff0508e06a';
+      const base_url = `https://api.openweathermap.org/data/2.5/forecast?q=${this.citySearch}&appid=${api_key}&units=metric`;
+
+        const response = await fetch(base_url);
+        const data = await response.json();
+        console.log(data);
+
+        this.citySearch = "";
+        this.weather.cityName = data.city.name;
+        this.weather.country = data.city.country;
+
+        // this.weather.icon = data.
+
+      
+
+      const today_dt = this.addDays(data.list[0].dt_txt, 0);
+
+      const data_today = data.list
+        .filter(function (entry) {
+          return (entry.dt_txt).includes(today_dt)
+        });
+
+      console.log(data_today);
+
+      const tomorrow_dt = this.addDays(data.list[0].dt_txt, 1);
+      
+      console.log(tomorrow_dt);
+      
+      const data_tmr = data.list.filter(function (entry) {
+        return (entry.dt_txt).includes(tomorrow_dt);
+      });
+
+      console.log(data_tmr);
+
+    },
+
+    addDays: function(date, days) {
+      var result = new Date(date);
+      result.setDate(result.getDate() + days);
+
+      const offset = result.getTimezoneOffset();
+      result = new Date(result.getTime() - (offset*60*1000));
+      return result.toISOString().split('T')[0];
+    },
+  },
+
+  components: {
+    CityCard,
+    WeatherView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import "./assets/style.css";
+
+</style>
---
+++ src/components/CityCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{}}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+      <div class="card-button">
+        <a href="#" class="btn btn-primary me-1">
+          Add City to Dashboard
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+          </svg>
+        </a>
+        
+        <a href="#" class="btn btn-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
